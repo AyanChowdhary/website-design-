a0fa367dc281a05d2c8a23a0c4d40b7c75b9ed60
AyanChowdhary <59221877+AyanChowdhary@users.noreply.github.com>
2020-11-29 00:53:28 +0530

    Initial commit

---
+++ src/qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fc2a31cb1c8af001eb58f44",
+				"5fc2a31cb1c8af001eb58f46"
+			]
+		},
+		"5fc2a31cb1c8af001eb58f44": {
+			"id": "5fc2a31cb1c8af001eb58f44",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fc2a31cb1c8af001eb58f46": {
+			"id": "5fc2a31cb1c8af001eb58f46",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app"
+}
